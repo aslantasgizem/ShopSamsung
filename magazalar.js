60632683b8e4af1d5da7843a2bae0f7d174b6053
aslantasgizem <50195160+aslantasgizem@users.noreply.github.com>
2019-11-17 15:45:42 +0300

    ShopSamsung/Odev4

--- magazalar.js
+++ magazalar.js
@@ -2,3 +2,11 @@ import Vue from 'vue'
 import VueSlideBar from 'vue-slide-bar'
  
 Vue.component('VueSlideBar', VueSlideBar)
+
+
+require("dotenv").config();
+
+import * as VueGoogleMaps from "vue2-google-maps";
+Vue.use(VueGoogleMaps, {
+  load: { key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY }
+});
